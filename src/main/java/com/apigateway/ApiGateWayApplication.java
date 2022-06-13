ea2e0673307b494594a5c7b8038f9f0da5dd9276
이호인 <lovi0714@kyonggi.ac.kr>
2022-06-13 22:37:25 +0900

    redis 설정

--- src/main/java/com/apigateway/ApiGateWayApplication.java
+++ src/main/java/com/apigateway/ApiGateWayApplication.java
@@ -5,9 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.cloud.netflix.zuul.filters.post.LocationRewriteFilter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 
-@SpringBootApplication
 @EnableZuulProxy
+@EnableRedisHttpSession
+@SpringBootApplication
 public class ApiGateWayApplication {
 
     public static void main(String[] args) {
